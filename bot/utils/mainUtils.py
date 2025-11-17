e693471cf2749e82e605f3582adc93f6edd506a0
Vitor <121695616+vitorhssv@users.noreply.github.com>
2025-11-17 00:45:44 -0300

    feat: Created the get_message util
    Gets a message from the database using the message identifier, also has the option to get a random message by using the command identifier

--- bot/utils/mainUtils.py
+++ bot/utils/mainUtils.py
@@ -1,4 +1,7 @@
 import logging
+import random
+import sqlite3
+from pathlib import Path
 
 
 def set_logger(module_name: str) -> logging.Logger:
@@ -7,3 +10,39 @@ def set_logger(module_name: str) -> logging.Logger:
 
 
 logger = set_logger("mainUtils")
+
+
+def get_message(identifier: str, get_random: bool = False) -> str | None:
+    """Gets a message from the database.\n\nWhen `get_random is False` the `identifier is message_id`.\n\nWhen `get_random is True` the `identifier is command_id`."""
+    try:
+        conn = sqlite3.connect(Path("botConfig.db"))
+        logger.info("Connected to database")
+        cursor = conn.cursor()
+
+        if get_random is False:
+            # gets a message using the message_id
+            cursor.execute(
+                "SELECT message FROM messages WHERE message_id = ?", (identifier,)
+            )
+
+            logger.info(f"Message from {identifier} fetched")
+            result = cursor.fetchone()[0]
+        elif get_random is True:
+            # gets a random message using the command_id
+            cursor.execute(
+                "SELECT message FROM messages WHERE command_id = ?", (identifier,)
+            )
+            result = random.choice(cursor.fetchall())[0]
+            logger.info(f"Messages from {identifier} fetched")
+
+        return result
+
+    except sqlite3.Error as e:
+        # log error
+        logger.error(e)
+
+    finally:
+        # close connection with the database
+        if "conn" in locals() and conn:
+            conn.close()
+            logger.info("Connection closed")
