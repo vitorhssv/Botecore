17741270ac365d433ea0603d82bd4ad8a7070bf3
Vitor <121695616+vitorhssv@users.noreply.github.com>
2025-11-20 22:26:17 -0300

    feat: Added /manage_permission_list
    This command lets the user add and remove users to the permission list

--- bot/modules/coreFunctions/__init__.py
+++ bot/modules/coreFunctions/__init__.py
@@ -8,12 +8,16 @@ if len(argv) > 1:
         database_update = False
 
 if not database_update:
-    this_module = import_module(".coreFunctions.commands", "modules")
+    commands = import_module(".coreFunctions.commands", "modules")
+    manage_permission_list = import_module(
+        ".coreFunctions.manage_permission_list", "modules"
+    )
     handlers = [
-        this_module.start_handler,
-        this_module.set_commands_handler,
-        this_module.reset_commands_handler,
-        this_module.restart_handler,
+        commands.start_handler,
+        commands.set_commands_handler,
+        commands.reset_commands_handler,
+        commands.restart_handler,
+        manage_permission_list.manage_permission_list_handler,
     ]
 else:
     commands_defaults = [
@@ -74,6 +78,74 @@ else:
                 {
                     "message_id": "coreFunctions:restart_error_message",
                     "message": "Something went wrong while restarting :\\",
+                },
+            ],
+        },
+        {
+            "command_id": "coreFunctions:manage_permission_list",
+            "command_handler": "manage_permission_list",
+            "command_description": "Manages the permission list",
+            "command_scope": "ownerOnly",
+            "messages": [
+                {
+                    "message_id": "coreFunctions:add_to_list",
+                    "message": "Add",
+                },
+                {
+                    "message_id": "coreFunctions:remove_from_list",
+                    "message": "Remove",
+                },
+                {
+                    "message_id": "coreFunctions:add_or_remove_choice",
+                    "message": "Do you want to add or remove contacts from the permission list?\n\nYou can /cancel this command if you want to",
+                },
+                {
+                    "message_id": "coreFunctions:add_contact_explanation",
+                    "message": "Send me the contacts you want to add!\n\nYou can /cancel this command if you want to",
+                },
+                {
+                    "message_id": "coreFunctions:add_contacts_button",
+                    "message": "Send the contacts",
+                },
+                {
+                    "message_id": "coreFunctions:users_added_successfully",
+                    "message": "All users added to the list!",
+                },
+                {
+                    "message_id": "coreFunctions:error_while_adding_users",
+                    "message": "Something went wrong while adding the users...",
+                },
+                {
+                    "message_id": "coreFunctions:no_users_were_sent",
+                    "message": "You didn't send me any users... Cancelling the command :T",
+                },
+                {
+                    "message_id": "coreFunctions:remove_contact_explanation",
+                    "message": "Alright! I'll send you a list of contacts in the bot, send me the number that identifies them separated by spaces (i.e.: 1 2 3 4 5) and I'll remove them!\n\nYou can /cancel this command if you want to",
+                },
+                {
+                    "message_id": "coreFunctions:users_removed_from_list",
+                    "message": "Done! Here's the updated list",
+                },
+                {
+                    "message_id": "coreFunctions:error_while_removing_users",
+                    "message": "Something went wrong while removing these users :/",
+                },
+                {
+                    "message_id": "coreFunctions:allowed_list_is_empty",
+                    "message": "The list is empty!",
+                },
+            ],
+        },
+        {
+            "command_id": "coreFunctions:cancel",
+            "command_handler": "cancel",
+            "command_description": "Cancels a function",
+            "command_scope": "nobody",
+            "messages": [
+                {
+                    "message_id": "coreFunctions:cancel",
+                    "message": "Command cancelled!",
                 },
             ],
         },
---
+++ bot/modules/coreFunctions/manage_permission_list.py
@@ -0,0 +1,255 @@
+import sqlite3
+from pathlib import Path
+
+from telegram import (
+    KeyboardButton,
+    KeyboardButtonRequestUsers,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    Update,
+)
+from telegram.ext import (
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+from utils import *
+
+logger = set_logger("coreFunctions:manage_permission_list")
+
+ADD_OR_REMOVE, ADD_CONTACTS, REMOVE_CONTACTS = range(3)
+
+
+def get_allowed_list(count: bool = False) -> str | None:
+    try:
+        result_message = "<blockquote>"
+        conn = sqlite3.connect(Path("botConfig.db"))
+        logger.info("Database connected")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM allowed WHERE 1 ORDER BY name ASC;")
+        list = cursor.fetchall()
+
+        user_count = 0
+        for user in list:
+            user_count += 1
+            result_message = (
+                result_message
+                + f"\n{f'{user_count} - ' if count is True else ''}<a href='tg://user?id={user[0]}'>{user[1]}</a>"
+            )
+
+        return result_message + "</blockquote>"
+
+    except sqlite3.Error as e:
+        # log error
+        logger.error(e)
+    finally:
+        # close connection with the database
+        if "conn" in locals() and conn:
+            conn.close()
+            logger.info("Connection closed")
+
+
+async def manage_permission_list(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Add or remove people who can use the bot"""
+    reply_keyboard = [
+        [
+            get_message("coreFunctions:add_to_list"),
+            get_message("coreFunctions:remove_from_list"),
+        ]
+    ]
+
+    await update.message.reply_html(
+        get_message("coreFunctions:add_or_remove_choice"),
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            one_time_keyboard=True,
+        ),
+    )
+    try:
+        await update.message.reply_html(get_allowed_list())
+    except:
+        await update.message.reply_html(
+            get_message("coreFunctions:allowed_list_is_empty")
+        )
+
+    return ADD_OR_REMOVE
+
+
+async def add_to_list_request(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Request to add a contact to the permission list"""
+    await update.message.reply_html(
+        get_message("coreFunctions:add_contact_explanation"),
+        reply_markup=ReplyKeyboardMarkup(
+            [
+                [
+                    KeyboardButton(
+                        get_message("coreFunctions:add_contacts_button"),
+                        request_users=KeyboardButtonRequestUsers(
+                            request_id=1,
+                            user_is_bot=False,
+                            request_name=True,
+                            max_quantity=10,
+                        ),
+                    )
+                ]
+            ],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        ),
+    )
+
+    return ADD_CONTACTS
+
+
+async def add_contacts_to_list(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Add the contacts to the permission list"""
+    users_shared = update.message.users_shared
+    if users_shared:
+        try:
+            conn = sqlite3.connect(Path("botConfig.db"))
+            logger.info("Database connected")
+            cursor = conn.cursor()
+
+            for user in users_shared.users:
+                cursor.execute(
+                    "SELECT id FROM allowed WHERE id = ?;",
+                    (user.user_id,),
+                )
+                user_in_db = cursor.fetchone()
+
+                if user_in_db is None:
+                    # if the user is not on the database, add them
+                    cursor.execute(
+                        "INSERT INTO allowed VALUES (?, ?);",
+                        (
+                            user.user_id,
+                            user.name,
+                        ),
+                    )
+
+            conn.commit()
+            await update.message.reply_text(
+                get_message("coreFunctions:users_added_successfully")
+            )
+        except sqlite3.Error as e:
+            # log error
+            logger.error(e)
+            await update.message.reply_text(
+                get_message("coreFunctions:error_while_adding_users")
+            )
+        finally:
+            # close connection with the database
+            if "conn" in locals() and conn:
+                conn.close()
+                logger.info("Connection closed")
+            try:
+                await update.message.reply_html(get_allowed_list())
+            except:
+                await update.message.reply_html(
+                    get_message("coreFunctions:allowed_list_is_empty")
+                )
+    else:
+        # no users sent
+        await update.message.reply_text(get_message("coreFunctions:no_users_were_sent"))
+
+    return ConversationHandler.END
+
+
+async def remove_from_list_request(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Request to remove the contacts from the permission list"""
+    await update.message.reply_html(
+        get_message("coreFunctions:remove_contact_explanation"),
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    # send the list with number counting each user
+    try:
+        await update.message.reply_html(get_allowed_list(True))
+    except:
+        await update.message.reply_html(
+            get_message("coreFunctions:allowed_list_is_empty")
+        )
+
+    return REMOVE_CONTACTS
+
+
+async def remove_contacts_from_list(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Remove contacts from the permission list"""
+    try:
+        conn = sqlite3.connect(Path("botConfig.db"))
+        logger.info("Database connected")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM allowed WHERE 1;")
+        users_list = cursor.fetchall()
+
+        to_remove = map(int, update.message.text.split(" "))
+
+        for index in to_remove:
+            if index == 1:
+                index = 0
+            else:
+                index -= 1
+            cursor.execute("DELETE FROM allowed WHERE id = ?", (users_list[index][0],))
+
+        conn.commit()
+    except sqlite3.Error as e:
+        # log error
+        logger.error(e)
+        await update.message.reply_text(
+            get_message("coreFunctions:error_while_removing_users")
+        )
+    finally:
+        # close connection with the database
+        if "conn" in locals() and conn:
+            conn.close()
+            logger.info("Connection closed")
+
+        await update.message.reply_text(
+            get_message("coreFunctions:users_removed_from_list")
+        )
+        try:
+            await update.message.reply_html(get_allowed_list())
+        except:
+            await update.message.reply_html(
+                get_message("coreFunctions:allowed_list_is_empty")
+            )
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancel the conversation"""
+    await update.message.reply_html(get_message("coreFunctions:cancel"))
+    return ConversationHandler.END
+
+
+handlers = get_handlers()
+manage_permission_list_handler = ConversationHandler(
+    entry_points=[
+        CommandHandler(
+            handlers["coreFunctions:manage_permission_list"], manage_permission_list
+        )
+    ],
+    states={
+        ADD_OR_REMOVE: [
+            MessageHandler(filters.Regex("(?i)^(add)$"), add_to_list_request),
+            MessageHandler(filters.Regex("(?i)^(remove)$"), remove_from_list_request),
+        ],
+        ADD_CONTACTS: [MessageHandler(filters.USER, add_contacts_to_list)],
+        REMOVE_CONTACTS: [MessageHandler(filters.TEXT, remove_contacts_from_list)],
+    },
+    fallbacks=[CommandHandler(handlers["coreFunctions:cancel"], cancel)],
+)
